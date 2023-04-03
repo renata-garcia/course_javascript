92d23193a0ed8ba5c7e293fd8a10f274c243076f
Renata Garcia <renata.garcia.eng@gmail.com>
2023-04-03 16:28:51 -0300

    functions

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,9 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function add(num1, num2) {
+    const result = num1 +  num2;
+    alert('The result is ' + result);
+}
+
+add(1,2);
+
 currentResult = currentResult + 10;
 
 let calculationDescription = `(${defaultResult} + 10)`;
-let errorMessage = 'An error ocurred!';
 
 outputResult(currentResult, calculationDescription);
