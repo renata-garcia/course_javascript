0192d7180204d347e3184a02ced3abc3cad2cfc9
Renata Garcia <renata.garcia.eng@gmail.com>
2023-04-03 17:19:46 -0300

    Global and Local Scope

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,12 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let result; //Global variable
 
 function add(num1, num2) {
-    const result = num1 +  num2;
-    return result;
+    const result = num1 +  num2; //Can access the global variable
+    return result; // Once there are a Global variable it is redundant
 }
 
 const additionResult = add(1,2);
+
+additionResult = result; //Bad code, bad uses of global variable
 
 currentResult = additionResult + currentResult + 10;
 
