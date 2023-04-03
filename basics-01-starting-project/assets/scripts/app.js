42d151ac830900f930cbb45eeb30f41ee0e1e64b
Renata Garcia <renata.garcia.eng@gmail.com>
2023-04-03 12:32:59 -0300

    Using Constant | More on Strings

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,8 +1,9 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
 currentResult = currentResult + 10;
 
-let calculationDescription = '(' + currentResult + ') + 10';
+let calculationDescription = `(${defaultResult} + 10)`;
+let errorMessage = 'An error ocurred!';
 
 outputResult(currentResult, calculationDescription);
-
