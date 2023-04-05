a6d890cdd1bb84dce3b7b31a3fbddbf01a13aefb
Renata Garcia <renata.garcia.eng@gmail.com>
2023-04-05 17:52:42 -0300

    Executing Functions "Indirectly" - this implementation is concatenate string and not adding number, it will be corrected in next step.

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,18 +1,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
-let result; //Global variable
 
 function add(num1, num2) {
-    const result = num1 +  num2; //Can access the global variable
-    return result; // Once there are a Global variable it is redundant
+    currentResult = currentResult +  userInput.value;
+    outputResult(currentResult, '');
 }
 
-const additionResult = add(1,2);
-
-additionResult = result; //Bad code, bad uses of global variable
-
-currentResult = additionResult + currentResult + 10;
-
-let calculationDescription = `(${defaultResult} + 10)`;
-
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener('click', add);
