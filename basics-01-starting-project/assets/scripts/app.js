df0fdf961b0e5f8b5d10615436313ef1d93ae78b
Renata Garcia <renata.garcia.eng@gmail.com>
2023-04-05 18:04:29 -0300

    Converting Data Type | Mixing the Numbers & String

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -2,7 +2,7 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add(num1, num2) {
-    currentResult = currentResult +  userInput.value;
+    currentResult = currentResult +  parseFloat(userInput.value);
     outputResult(currentResult, '');
 }
 
