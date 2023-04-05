b9a0ce096f53453c54caffff6e17f7bb2a3ed064
Renata Garcia <renata.garcia.eng@gmail.com>
2023-04-05 18:06:56 -0300

    Splitting Code Into Functions

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,9 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
 function add(num1, num2) {
-    currentResult = currentResult +  parseFloat(userInput.value);
-    outputResult(currentResult, '');
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} + ${enteredNumber}`;
+    currentResult = currentResult +  parseFloat(enteredNumber);
+    outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add);
